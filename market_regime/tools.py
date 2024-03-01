a8d8f07c33a809cc8fa19d709760bcd87419811b
SIusifov <157707968+SIusifov@users.noreply.github.com>
2024-03-01 22:51:22 +0100

    Update tools.py

--- market_regime/tools.py
+++ market_regime/tools.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 def figures_to_html(figs, filename="dashboard.html"):
-    with open(filename, 'w') as dashboard:
+    with open(filename, 'w', encoding="utf-8") as dashboard:
         dashboard.write("<html><head></head><body>" + "\n")
         for fig in figs:
             inner_html = fig.to_html().split('<body>')[1].split('</body>')[0]
